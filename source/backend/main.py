13563c310e0ccc4c580234a311e746894c9100aa
rtudorica <rtudorica@bitdefender.com>
2022-11-28 21:22:54 +0200

    added the entry service

--- source/backend/main.py
+++ source/backend/main.py
@@ -3,6 +3,7 @@ sys.path.insert(1, "controllers")
 
 from concurrent.futures import ThreadPoolExecutor
 from controllers.user_service import UserService
+from controllers.entry_service import EntryService
 import grpc
 import controllers.v1_pb2_grpc as ApiSpec
 
@@ -10,8 +11,9 @@ def main():
 	server = grpc.server(ThreadPoolExecutor(max_workers = 16))
 
 	ApiSpec.add_UserServiceServicer_to_server(UserService(), server)
-	ApiSpec.add_EntryServiceServicer_to_server(ApiSpec.EntryServiceServicer(), server)
+	ApiSpec.add_EntryServiceServicer_to_server(EntryService(), server)
 	ApiSpec.add_BATServiceServicer_to_server(ApiSpec.BATServiceServicer(), server)
+	ApiSpec.add_CTRUServiceServicer_to_server(ApiSpec.CTRUServiceServicer(), server)
 
 	server.add_insecure_port("[::]:5000")
 	server.start()
